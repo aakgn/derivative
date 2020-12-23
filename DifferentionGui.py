804e3a97a602e3663f85c15a6396d0e32460e6d9
aakgn <75807351+aakgn@users.noreply.github.com>
2020-12-23 23:13:08 +0300

    Add files via upload

---
+++ DifferentionGui.py
@@ -0,0 +1,17 @@
+# @author: ali.akgun
+# @to do:
+# @bugs:
+# @brief Differention gui
+# @date: 23.12.2020
+
+from tkinter import *
+
+# create window object
+
+app = Tk()
+
+app.title("Differention")
+app.geometry("700x350")
+
+# start program
+app.mainloop()
